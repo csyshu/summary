650a39433091c38efcbe59de82108a8bf4765e1d
shuyun.cheng <shuyun.cheng@100credit.com>
2021-07-26 15:38:40 +0800

    :sparkles: elasticjob demo

---
+++ src/main/java/com/csy/summary/daily/elasticjob/ElasticJobRegistryCenterConfig.java
@@ -0,0 +1,38 @@
+package com.csy.summary.daily.elasticjob;
+
+import com.dangdang.ddframe.job.reg.base.CoordinatorRegistryCenter;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperConfiguration;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author shuyun.cheng
+ */
+@Configuration
+public class ElasticJobRegistryCenterConfig {
+
+    /**
+     * zookeeper链接字符串 localhost:2181
+     */
+    private static final String ZOOKEEPER_CONNECTION_STRING = "localhost:2181";
+    /**
+     * 定时任务命名空间
+     */
+    private static final String JOB_NAMESPACE = "elastic-job-boot-java";
+
+    /**
+     * zk的配置及创建注册中心
+     *
+     * @return CoordinatorRegistryCenter
+     */
+    @Bean(initMethod = "init")
+    public CoordinatorRegistryCenter setUpRegistryCenter() {
+        //zk的配置
+        ZookeeperConfiguration zookeeperConfiguration = new ZookeeperConfiguration(ZOOKEEPER_CONNECTION_STRING, JOB_NAMESPACE);
+        zookeeperConfiguration.setSessionTimeoutMilliseconds(1000);
+        //创建注册中心
+        return new ZookeeperRegistryCenter(zookeeperConfiguration);
+
+    }
+}
---
+++ src/main/java/com/csy/summary/daily/elasticjob/FileService.java
@@ -0,0 +1,45 @@
+package com.csy.summary.daily.elasticjob;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author shuyun.cheng
+ */
+@Service
+public class FileService {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    /**
+     * 获取某文件类型未备份的文件
+     * @param fileType 文件类型
+     * @param count 获取条数
+     * @return List<FileCustom>
+     */
+    public List<FileCustom> fetchUnBackupFiles(String fileType, Integer count){
+        String sql="select * from t_file where type = ? and backedUp = 0 limit 0,?";
+        return jdbcTemplate.query(sql, new Object[]{fileType, count}, new BeanPropertyRowMapper(FileCustom.class));
+    }
+
+    /**
+     * 备份文件
+     * @param files 要备份的文件
+     */
+    public void backupFiles(List<FileCustom> files){
+        for(FileCustom fileCustom:files){
+            String sql="update t_file set backedUp = 1 where id = ?";
+            jdbcTemplate.update(sql,new Object[]{fileCustom.getId()});
+            System.out.println(String.format("线程 %d | 已备份文件:%s  文件类型:%s"
+                    ,Thread.currentThread().getId()
+                    ,fileCustom.getName()
+                    ,fileCustom.getType()));
+        }
+
+    }
+}
